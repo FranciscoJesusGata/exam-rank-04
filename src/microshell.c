28671f53fab89beb3cfdbb420a75a295d3d73467
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2022-03-23 19:07:13 +0100

    tmp is copied only once, code a bit shorter

--- src/microshell.c
+++ src/microshell.c
@@ -1,56 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   microshell2.c                                      :+:      :+:    :+:   */
+/*   microshell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 20:18:25 by fgata-va          #+#    #+#             */
-/*   Updated: 2022/03/23 12:14:14 by fgata-va         ###   ########.fr       */
+/*   Created: 2022/03/23 16:55:21 by fgata-va          #+#    #+#             */
+/*   Updated: 2022/03/23 19:00:40 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <string.h>
 
 void	ft_puterr(char *s, char *name) {
-	write(2, "error: ", 7);
+	write(1, "error: ", 7);
 	while (*s)
-		write(2,s++,1);
+		write(2, s++, 1);
 	write(2, " ", 1);
 	while (*name)
-		write(2,name++,1);
-	write(2, "\n",1);
+		write(2, name++, 1);
+	write(2, "\n", 1);
 	exit(1);
 }
 
 pid_t	execute(int first, int second, int io, char *argv[], char *const envp[]) {
-	pid_t	pid;
-
-	pid = fork();
+	pid_t	pid = fork();
 	if (!pid) {
-		close(first);
-		if (second >= 0) {
-			if ((dup2(second, io)) < 0)
-				ft_puterr("fatal", NULL);
-		}
-		if (!(*argv))
+		close (first);
+		if (second >= 0 && (dup2(second, io)) < 0)
+			ft_puterr("fatal", NULL);
+		if (!argv[0])
 			exit(0);
-		execve(*argv, argv, envp);
-		ft_puterr("cannot execute", *argv);
+		if ((execve(*argv, argv, envp)) < 0)
+			ft_puterr("cannot execute", *argv);
 	}
 	return (pid);
 }
 
-int main(int argc, char *argv[], char *const envp[]) {
+int	main(int argc, char *argv[], char *envp[]) {
 	int		i;
-	int		exit_code = 0;
 	pid_t	pid;
+	int		exit_code = 0;
 	int		fds[2] = {-1, -1};
-	int		tmp = -1;
+	int		tmp = dup(STDIN_FILENO);
 
 	(void)argc;
 	argv++;
@@ -58,40 +53,42 @@ int main(int argc, char *argv[], char *const envp[]) {
 		i = 0;
 		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
 			i++;
-		if (!strcmp(*argv, "cd")) {
-			if (i != 2)
+		if (!(strcmp(*argv, "cd"))) {
+			if (i != 2) {
+				close(tmp);
 				ft_puterr("cd:", "bad arguments");
-			else if ((chdir(argv[1])) < 0)
+			} else if ((chdir(argv[1])) < 0) {
+				close(tmp);
 				ft_puterr("cd: cannot change directory to", argv[1]);
-			argv += i;
-		} else if (argv[i] && !strcmp(argv[i], "|")) {
-			argv[i++] = 0;
+			}
+		}
+		else if (argv[i] && !(strcmp(argv[i], "|"))) {
+			argv[i++] = NULL;
 			if (fds[0] >= 0) {
-				tmp = dup(STDIN_FILENO);
-				dup2(fds[0], STDIN_FILENO);
+				if ((dup2(fds[0], STDIN_FILENO)) < 0) {
+					close(tmp);
+					close(fds[0]);
+					ft_puterr("fatal", NULL);
+				}
 				close(fds[0]);
 			}
 			pipe(fds);
 			pid = execute(fds[0], fds[1], STDOUT_FILENO, argv, envp);
-			close(fds[1]);
-			if (tmp >= 0) {
-				dup2(tmp, STDIN_FILENO);
-				close(tmp);
-				tmp = -1;
-			}
+			dup2(tmp, STDIN_FILENO);
 			waitpid(pid, &exit_code, 0);
-			argv += i;
+			close(fds[1]);
 		} else {
-			if (argv[i] && !strcmp(argv[i], ";"))
-				argv[i++] = 0;
+			if (argv[i] && !(strcmp(argv[i], ";")))
+				argv[i++] = NULL;
 			pid = execute(fds[1], fds[0], STDIN_FILENO, argv, envp);
 			close(fds[0]);
 			fds[0] = -1;
 			close(fds[1]);
 			fds[1] = -1;
 			waitpid(pid, &exit_code, 0);
-			argv += i;
 		}
+		argv += i;
 	}
+	close(tmp);
 	return (WEXITSTATUS(exit_code));
 }
